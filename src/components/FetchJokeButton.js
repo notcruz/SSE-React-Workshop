bc9a449847134cbf35690bfe40bd0e9cb0f5a0c5
jc1831 <jc1831@rit.edu>
2023-03-22 18:20:10 -0400

    Initial Commit

---
+++ src/components/FetchJokeButton.js
@@ -0,0 +1,7 @@
+export default function FetchJokeButton() {
+    return (
+        <button className={"bg-indigo-500 rounded text-white font-semibold px-3 py-2 transition-all duration-300 hover:bg-indigo-600"}>
+            Fetch A New Joke
+        </button>
+    )
+}
---
+++ src/components/RevealJokeButton.js
@@ -0,0 +1,7 @@
+export default function RevealJokeButton() {
+    return (
+        <button className={"bg-indigo-500 rounded text-white font-semibold px-3 py-2 transition-all duration-300 hover:bg-indigo-600"}>
+            Reveal Joke
+        </button>
+    )
+}
